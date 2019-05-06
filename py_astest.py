67ccf0802ec304cab654c246e2c88894da11a824
unknown <wenc@CETCA.NET.CN>
2019-05-06 17:22:33 +0800

    start up shell

--- py_astest.py
+++ py_astest.py
@@ -1,7 +1,14 @@
 import os
+import time
+os.mkdir("testlog")
 cmd="sh ssdtest.sh"
+fmt="%Y-%m-%d %X"
 ret=os.system(cmd)
 if ret==0:
 	pass
 else:
-	print("ssd test wrong")
+	print(time.strftime(fmt)+" ssd test wrong")
+	with open("./testlog/log","a") as f:
+		f.write(time.strftime(fmt)+" ssd test wrong\n")
+		f.flush()
+
