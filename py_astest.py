55d7759ff5b1604521b760bf21cdcee22d9b1543
unknown <wenc@CETCA.NET.CN>
2019-05-07 16:55:54 +0800

    5.7 add def ssd_shell()

--- py_astest.py
+++ py_astest.py
@@ -1,14 +1,19 @@
 import os
 import time
-os.mkdir("testlog")
-cmd="sh ssdtest.sh"
-fmt="%Y-%m-%d %X"
-ret=os.system(cmd)
-if ret==0:
-	pass
-else:
-	print(time.strftime(fmt)+" ssd test wrong")
-	with open("./testlog/log","a") as f:
-		f.write(time.strftime(fmt)+" ssd test wrong\n")
-		f.flush()
+
+def ssd_shell():
+	os.mkdir("testlog")
+	cmd="sh ssdtest.sh"
+	fmt="%Y-%m-%d %X"
+	ret=os.system(cmd)
+	if ret==0:
+		pass
+	else:
+		print(time.strftime(fmt)+" ssd test wrong")
+		with open("./testlog/log","a") as f:
+			f.write(time.strftime(fmt)+" ssd test wrong\n")
+			f.flush()
+
+
+
 
