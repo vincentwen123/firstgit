be7044a86b3257b0c849b8d6b1c1258f027965cd
unknown <wenc@CETCA.NET.CN>
2019-05-15 14:56:44 +0800

    ssd测试脚本和集成ssd测试脚本

--- py_astest.py
+++ py_astest.py
@@ -1,18 +1,40 @@
-import os
-import time
+import os,sys,shutil,filecmp,time,gevent,ctypes
+from ctypes import *
 
-def ssd_shell():
-	os.mkdir("testlog")
-	cmd="sh ssdtest.sh"
+
+
+def ssd_shell_1():
+	i=0
 	fmt="%Y-%m-%d %X"
-	ret=os.system(cmd)
-	if ret==0:
+	tpath="./ssd_test2"
+	spath="./ssd_test1"
+	sfile=spath+"/tmp.file1"
+	tfile=tpath+"/tmp.file_"
+	logpath=spath+"/ssd_log"
+	try:
+		os.mkdir(spath)
+		os.mkdir(tpath)
+	except FileExistsError as e:
+		print(e)
+	with open(sfile,"w") as f:
 		pass
-	else:
-		print(time.strftime(fmt)+" ssd test wrong")
-		with open("./testlog/log","a") as f:
-			f.write(time.strftime(fmt)+" ssd test wrong\n")
-			f.flush()
+	os.system("dd if=/dev/urandom of=./ssd_test1/tmp.file1 bs=1M count=10 >/dev/null 2>&1")
+
+	while True:
+		time.sleep(0.5)#change to gevent.sleep(0.5)
+		ttfile=tfile+str(i)
+		shutil.copy(sfile,ttfile)
+		i+=1
+		if filecmp.cmp(sfile,ttfile,False):
+			print(time.strftime(fmt)+" ssd success")
+			with open(logpath,"a") as f:
+				f.write(time.strftime(fmt)+" ssd normal\n")
+				f.flush()
+		else:
+			print(time.strftime(fmt)+"ssd wrong")
+			with open(logpath,"a") as f:
+				f.write(time.strftime(fmt)+"ssd wrong\n")
+				f.flush()
 
 
 
